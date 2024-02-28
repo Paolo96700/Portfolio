09b3b251ccda10727629fc5c67624152f280b8aa
Paolo Falco <paolo.falcoapp@gmail.com>
2024-02-28 15:43:07 +0100

    add AOS for animation

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import AOS from 'aos';
+import "aos/dist/aos.css"
 
 import "./style.css";
 import "flowbite";
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router, AOS.init())
 
 app.mount('#app')
